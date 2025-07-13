3682b65e6579469f686c52f1267e6d1533d1bbde
Vighnesh-code <vighneshzagade4@gmail.com>
2025-07-13 14:43:50 +0530

    next js app initialization

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='h-screen w-screen'>
+      <h1 className='font-karla text-3xl'>Welcome to the LOOM Clone</h1>
+    </div>
+  )
+}
+
+export default Page
